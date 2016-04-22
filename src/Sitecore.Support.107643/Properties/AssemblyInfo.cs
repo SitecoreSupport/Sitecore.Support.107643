95ef2fe7d7a8a2f05eebf55602d54e7031989f9e
Oleg Burov <oleg.burov@outlook.com>
2016-04-22 13:41:52 -0700

    Implemented Sitecore Patch #107643.

---
+++ src/Sitecore.Support.107643/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.107643")]
+[assembly: AssemblyProduct("Sitecore.Support.107643")]
+[assembly: AssemblyVersion("8.1.2.0")]
+[assembly: AssemblyFileVersion("8.1.2.0")]
+[assembly: AssemblyInformationalVersion("1.0.0 rev. 160422")]
+
+[assembly: AssemblyDescription("Sitecore Support Patch #107643.")]
+[assembly: AssemblyCompany("Sitecore Corporation")]
+[assembly: AssemblyCopyright("Copyright © 2016 Sitecore Corporation")]
+[assembly: AssemblyTrademark("Sitecore® is a registered trademark of Sitecore Corporation")]
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74d14f67-65f4-4d88-b0c6-bfac1ab7142a")]
